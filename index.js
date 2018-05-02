335183a52c10b29bbe815d6265ffed8a42a13f44
Generalwrex <bjdroot@gmail.com>
2018-05-02 15:53:47 -0700

    latest progress commit, nothing is still set in stone

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 #!/usr/bin/env node
 //DBM CLI Tool by General Wrex under MIT
 
-const argv = require('minimist')(process.argv.slice(2));
+const dbmcli = {}
+
+dbmcli.args = require('minimist')(process.argv.slice(2));
 
 const chalk       = require('chalk');
 const clear       = require('clear');
@@ -10,11 +12,21 @@ const CLI         = require('clui');
 const inquirer    = require('inquirer');
 const Spinner     = CLI.Spinner;
 
-// load lib modules
-const utils = require('./lib/utils');
-const files = require('./lib/files');
-const config = require('./lib/config');
-const commands = require('./lib/commands');
+// load base modules
+const config = dbmcli.config  = require('./lib/config');
+const log    = dbmcli.log     = require('./lib/log');
+const utils  = dbmcli.utils   = require('./lib/utils');
+
+// load core modules
+const files  = dbmcli.files   = require('./lib/files')(dbmcli);
+const bots  = dbmcli.bots   = require('./lib/bots')(dbmcli);
+
+if(!files.directoryExists(files.getDataDirectory())){
+
+    console.log(chalk.red("This tool must be ran from the root directory of a Discord Bot created with DBM."));
+    console.log(chalk.white(files.getDataDirectory()), chalk.red("doesn't seem to be a valid path"));
+    process.exit(1);
+}
 
 
 ///////////////////BANNER////////////////////////////////////////////////
@@ -22,12 +34,14 @@ clear();
 console.log(utils.localISOString())
 console.log(chalk.yellow(figlet.textSync('Discord Bot Maker', { horizontalLayout: 'full' })));
 console.log(chalk.yellow(figlet.textSync('CLI Tool', { horizontalLayout: 'full' })));
-console.log(chalk.green(config.version));
+console.log('Version: ',chalk.green(config.get('app:version')));
+
+
 
 /////////////////////////////////////////////////////////////////////////
 
+if(!config.get('initial:run')){
 
-if(!config.firstRun){
     console.log(chalk.red("It seems this is the first time you've ran the tool, we're setting it up!"));
 
     const status = new Spinner('This may take a few minutes...');
@@ -36,31 +50,23 @@ if(!config.firstRun){
     // do first run stuff here
 
     status.stop();
-    config.setFirstRun(utils.localISOString());
+    config.set('app:firstRun',utils.localISOString());
 }
 
 
-let commandArray = commands.load(files.getBotCommands())
-
-console.log(chalk.green(`Found ${commandArray.length} commands`));
-
 console.log(chalk.white(`Welcome! Pick an option below to get started! (Red Features = Not Ready Yet)\r\n`));
-
-
 console.log(chalk.green(`DBM Mods Management Features;\r\n`));
-console.log(chalk.red("Install"),chalk.white("-Install normal or beta mods to the provided bot location."))
-console.log(chalk.red("Uninstall"),chalk.white("-Uninstall mods from the selected bot."))
-console.log(chalk.red("Sync"),chalk.white("-Sync mods between multiple bot locations."))
+console.log(chalk.red("Install Mods")  ,chalk.white("-Download and Install normal or beta mods to the provided bot location."))
+console.log(chalk.red("Update Mods")   ,chalk.white(" -Download and Install normal or beta mods to the provided bot location."))
+console.log(chalk.red("Uninstall Mods"),chalk.white("-Uninstall mods from the selected bot."))
+console.log(chalk.red("Sync Mods")     ,chalk.white("-Sync mods between multiple bot locations."))
 console.log(chalk.red("View Changelog"),chalk.white("-View the latest master and beta changelogs."))
 console.log("-------------------------------------------------------------------------------------");
 console.log(chalk.green(`\r\nDBM Commands and Events Management Features;\r\n`));
-console.log(chalk.red("Backup"),chalk.white("-Backup the commands of the selected bot to a json file "))
-console.log(chalk.red("Merge"),chalk.white("-Merge commands or events from one file to another."))
-console.log(chalk.red("Export"),chalk.white("-Export all commands or events to the specified location."))
-console.log(chalk.red("Import"),chalk.white("-Import all commands or events to the specified location. Overwriting is optional."))
-
-// ready ones
-console.log(" ");
+console.log(chalk.red("Backup") ,chalk.white("-Backup the commands of the selected bot to a json file "))
+console.log(chalk.red("Merge")  ,chalk.white("-Merge commands or events from one file to another."))
+console.log(chalk.red("Export") ,chalk.white("-Export selected commands or events to the specified location."))
+console.log(chalk.red("Import") ,chalk.white("-Import selected commands or events to the specified location. Overwriting is optional."))
 
 function test() {
     inquirer
